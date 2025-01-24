ef8efef7c01b99a6ffcfddde04a6f9dc013fe886
Senn <purpleface@pm.me>
2025-01-25 00:08:15 +0530

    Player global_position clamp

--- src/Player/player.gd
+++ src/Player/player.gd
@@ -5,6 +5,7 @@ class_name  Player extends CharacterBody2D
 @export var movement_weight: float = 20.0
 
 func _ready() -> void:
+	print(get_viewport_rect().size)
 	pass
 
 func _physics_process(delta: float) -> void:
@@ -13,4 +14,6 @@ func _physics_process(delta: float) -> void:
 		velocity = lerp(velocity, speed * direction, movement_weight * delta)
 	else:
 		velocity = lerp(velocity, Vector2.ZERO, movement_weight / 2 * delta)
+	
 	move_and_slide()
+	global_position = global_position.clamp(Vector2.ZERO, get_viewport_rect().size)
