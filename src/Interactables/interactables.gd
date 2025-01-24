3fc85a115a742b75ed1d55d93b4c4f810830a0fe
Madhavaprasath23 <156769328+Madhavaprasath23@users.noreply.github.com>
2025-01-25 00:40:34 +0530

    Generation Init code

--- src/Interactables/interactables.gd
+++ src/Interactables/interactables.gd
@@ -1,15 +1,23 @@
 extends Area2D
 
 @export var Gravity :int= 10
+@onready var Level = get_tree().get_nodes_in_group("Level")[0]
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
 	
 
 func _physics_process(delta: float) -> void:
 	global_position.y+= Gravity*delta
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	Level.remove_from_position(self)
+	queue_free()
+
+
+func _on_visible_on_screen_notifier_2d_screen_entered() -> void:
+	pass # Replace with function body.
