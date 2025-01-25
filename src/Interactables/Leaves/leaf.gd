81296ad7fb8c92c0b2b5e04abdfc74612920a0c6
Madhavaprasath23 <156769328+Madhavaprasath23@users.noreply.github.com>
2025-01-25 09:32:46 +0530

    Testing and leaffinalization

--- src/Interactables/Leaves/leaf.gd
+++ src/Interactables/Leaves/leaf.gd
@@ -4,15 +4,15 @@ extends Node2D
 var target_moving_speed = 64
 var steering = Vector2()
 var velocity = Vector2()
-var max_force = 500
-var target = Vector2(randf_range(600,1200),randf_range(700+64,800+64))
+var max_force = 100
+@onready var target = Vector2(randf_range(600,1200),randf_range(700+64,800+64))
+var spwan_position = null
 func _ready() -> void:
 	randomize()
-	target = Vector2(randf_range(600,1200),randf_range(700+64,800+64))
 	print(target)
 func _physics_process(delta: float) -> void:
-	velocity = (target - position).normalized() * 700
-	var desired_velocity = (target - position).normalized() * 700
+	velocity = (target - position).normalized() * 100
+	var desired_velocity = (target - position).normalized() * 100
 
 	steering = desired_velocity - velocity
 	steering = steering.limit_length(max_force)
